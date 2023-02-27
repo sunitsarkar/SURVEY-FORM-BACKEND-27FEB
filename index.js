8dfd5f17ff72cb1dbaa201073da4e9b089837754
sunitsarkar <sunit6503@gmail.com>
2023-02-27 22:49:55 +0530

    fixed

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const express = require('express');
 const cors = require("cors");
 const bodyparser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const registrationRouter=require('./route/route')
 
 const app = express();
+const uri = "mongodb+srv://sunitsarkar:LwP8bgRq3VOKlHWI@cluster0.gxschpx.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("CONNECTED TO DATABASE");
+    });
+
 app.use(cors());
-app.use(bodyparser.urlencoded({extended : false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 app.use(cookieParser());
-
+app.use('/',registrationRouter)
 
 const port = 8000;
 
@@ -17,41 +30,9 @@ app.listen(port, () => {
     console.log("server is live....")
 });
 
-//
 
-// const books = [
-//     { title: 'Harry Potter', id: 1 },
-//     { title: 'Twilight', id: 2 },
-//     { title: 'Lorien Legacies', id: 3 }
-// ]
 
-// //READ 
-// app.get('/', (req, res) => {
-//     res.send('Welcome to REST API with Node.js !!');
-// });
 
-// app.get('/api/books', (req, res) => {
-//     res.send(books);
-// });
-
-// app.get('/api/books/:id', (req, res) => {
-//     const book = books.find(c => c.id === parseInt(req.params.id));
-
-//     if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
-//     res.send(book);
-// });
-
-// app.post("/signin", (req, res) => {
-//     const email = req.body.email;
-//     const password = req.body.password;
-
-//     if (!email || !password) {
-//         res.status(400).send("Error!1")
-//     } else {
-//         res.send(email && password)
-//     }
-
-// });
 
 
 
--- route/route.js
+++ route/route.js
@@ -1,32 +1,38 @@
 const express = require('express');
 
 const router = express.Router();
-const user = require('../model/user');
+const User = require('../model/user');
 
-router.post('/', () => {
+router.post('/', (req,res) => {
+    const user=new User(req.body);
 
+    user.save((err,success)=>{
+        res.send({success})
+    })
 })
 
 /*
 this is to store the user data when one tries to registrer.....soo we need only one methode that is post.
 */
 
+module.exports=router;
 
 
-// Implement the POST API - to creating the new survey
-router.post("/surveys", (req, res) => {
-    const survey = req.body;
-    // save the survey to the database
-    res.send("Survey created successfully!!");
-});
+
+// // Implement the POST API - to creating the new survey
+// router.post("/surveys", (req, res) => {
+//     const survey = req.body;
+//     // save the survey to the database
+//     res.send("Survey created successfully!!");
+// });
 
 
-// GET API for existing survey
-router.get("/surveys:id", (req, res) => {
-    const surveyId = req.params.id;
-    // fetch the survey from the database using the surveyId
-    const survey = { id: surveyId, title: 'Sample survey', questions: [] };
-    res.send(survey);
-});
+// // GET API for existing survey
+// router.get("/surveys:id", (req, res) => {
+//     const surveyId = req.params.id;
+//     // fetch the survey from the database using the surveyId
+//     const survey = { id: surveyId, title: 'Sample survey', questions: [] };
+//     res.send(survey);
+// });
 
 
