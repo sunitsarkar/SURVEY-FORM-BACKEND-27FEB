833572cece55b22b7cd37f41070b7808ea7c2c29
sunitsarkar <sunit6503@gmail.com>
2023-02-27 18:14:26 +0530

    route added

---
+++ route/route.js
@@ -0,0 +1,12 @@
+const express=require('express');
+
+const router=express.Router();
+const user=require('../model/user');
+
+router.post('/',()=>{
+
+})
+
+/*
+this is to store the user data when one tries to registrer.....soo we need only one methode that is post.
+*/
