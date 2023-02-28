0d5db4982f8ab663149d8cb87eeeadfe02330aa6
sunitsarkar <sunit6503@gmail.com>
2023-02-28 15:31:18 +0530

    survey api completed

--- route/questionRoute.js
+++ route/questionRoute.js
@@ -13,46 +13,8 @@ router.post('/create', (req, res) => {
 })
 
 
-// to read
-router.get('/', async (req, res) => {
-    try {
-        const question = await Question.findOne({ name: req.query.name, question: req.query.question });
-        if (!question) {
-            return res.status(404).json({ message: "Question not found" });
-        }
-        res.status(200).json(question);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
 
 
-// to Update
-router.patch('/', async(req, res) => {
-    try {
-        const question = await Question.findOneAndUpdate({ name: req.query.name, question: req.query.question }, req.body, { new: true });
-        if (!question) {
-            return res.status(404).json({ message: "Question not found" });
-        }
-        res.status(200).json(question);
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
-
-
-// to delete
-router.delete('/', async (req, res) => {
-    try {
-        const question = await Question.findOneAndDelete({ name: req.query.name, question: req.query.question });
-        if (!question) {
-            return res.status(404).json({ message: "Question not found" });
-        }
-        res.status(200).json({ message: "Question deleted successfully" });
-    } catch (err) {
-        res.status(400).json({ message: err.message });
-    }
-});
 
 
 module.exports = router;
--- route/surveyRoute.js
+++ route/surveyRoute.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const router = express.Router();
 const UserSurvey = require('../model/userSurvey');
 
@@ -13,6 +12,25 @@ router.post('/create', (req, res) => {
     })
 });
 
+router.param("name", (req, res, next, name) => {
+  UserSurvey.find({name:name}).exec((err, surveys) => {
+    // error checking
+    if (err || !surveys) {
+      return res.status(400).json({
+        error: `Something went wrong in finding ${name}  surveys`,
+      });
+    }
+    // return all the todos in json format
+    req.survey=surveys;
+    // res.json(surveys);
+    next();
+  });
+});
+
+router.get('/mario',(req,resp)=>{
+  resp.send(marioModel.mariochar)
+});
+
 
 //to read all surveys to render on login
 router.get('/surveys',  (req, res) => {
@@ -21,7 +39,7 @@ router.get('/surveys',  (req, res) => {
       // error checking
       if (err || !surveys) {
         return res.status(400).json({
-          error: "Something went wrong in finding all todos",
+          error: "Something went wrong in finding all surveys",
         });
       }
       // return all the todos in json format
@@ -32,18 +50,44 @@ router.get('/surveys',  (req, res) => {
 
 //not finished******************************************
 router.get('/surveys/:name',(req,res)=>{
-    const name=req.body.name;
-    console.log(name)
-    UserSurvey.find({name:name}).exec((err, surveys) => {
-        // error checking
-        if (err || !surveys) {
-          return res.status(400).json({
-            error: "Something went wrong in finding all todos",
-          });
-        }
-        // return all the todos in json format
-        res.json(surveys);
+    res.json(req.survey)
+});
+
+router.delete('/surveys/:name/delete',(req,res)=>{
+  const survey=req.survey[0];
+  // console.log(survey)
+  survey.remove((err, task) => {
+    if (err || !task) {
+      return res.status(400).json({
+        error: "something went wrong while deleting the category",
       });
+    }
+    // send deleted todo and success message as a json response
+    res.json({
+      deleted: survey,
+      message: "survey deleted successfully!",
+    });
+  })
+});
+
+
+//a user can only update survey name,description and other crietria
+
+router.put('/surveys/:name/update',(req,res)=>{
+  const survey=req.survey[0];
+  survey.name=req.body.name;
+  survey.description=req.body.description;
+  survey.otherCrietria=req.body.otherCrietria;
+
+  survey.save((err, t) => {
+    if (err || !t) {
+      return res.status(400).json({
+        error: "something went wrong while updating",
+      });
+    }
+    // send the updated todo as a json response
+    res.json(t);
+  });
 })
 
 //********************************************************* */
