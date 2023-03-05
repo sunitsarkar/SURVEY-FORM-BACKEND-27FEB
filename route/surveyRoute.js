1c10d1ee92d753dc3d07c33aa09ca5d27fc10f0e
sunitsarkar <sunit6503@gmail.com>
2023-03-05 21:33:11 +0530

    delete

--- route/surveyRoute.js
+++ route/surveyRoute.js
@@ -13,12 +13,12 @@ router.post('/create', (req, res) => {
 
 
 
-router.param("name", (req, res, next, name) => {
-  UserSurvey.find({name:name}).exec((err, surveys) => {
+router.param("id", (req, res, next, id) => {
+  UserSurvey.find({_id:id}).exec((err, surveys) => {
     // error checking
     if (err || !surveys) {
       return res.status(400).json({
-        error: `Something went wrong in finding ${name}  surveys`,
+        error: `Something went wrong in finding surveys`,
       });
     }
     // return all the todos in json format
@@ -45,12 +45,12 @@ router.get('/surveys',  (req, res) => {
 });
 
 //not finished******************************************
-router.get('/surveys/:name',(req,res)=>{
+router.get('/surveys/:id',(req,res)=>{
     res.json(req.survey)
 });
 
-router.delete('/surveys/:name/delete',(req,res)=>{
-  console.log(req)
+router.delete('/surveys/:id/delete',(req,res)=>{
+  console.log(req.survey[0]);
   const survey=req.survey[0];
   // console.log(survey)
   survey.remove((err, task) => {
@@ -67,10 +67,9 @@ router.delete('/surveys/:name/delete',(req,res)=>{
   })
 });
 
-
 //a user can only update survey name,description and other crietria
 
-router.put('/surveys/:name/update',(req,res)=>{
+router.put('/surveys/:id/update',(req,res)=>{
   const survey=req.survey[0];
   survey.name=req.body.name;
   survey.description=req.body.description;
